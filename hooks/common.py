38c6bab4fa928dc0aa290f1fdbfce112514deac4
Clint Byrum <clint@ubuntu.com>
2011-05-31 23:33:06 -0700

    fixing departed/broken behavior

--- hooks/common.py
+++ hooks/common.py
@@ -2,8 +2,17 @@
 
 import os
 import MySQLdb
+import subprocess
 
-change_unit = os.environ.get("ENSEMBLE_REMOTE_UNIT")
+try:
+    change_unit = os.environ['ENSEMBLE_REMOTE_UNIT']
+except KeyError:
+    pass
+
+if len(change_unit) == 0:
+    # XXX hack to work around https://launchpad.net/bugs/791042
+    change_unit  = subprocess.check_output(['relation-list']).strip().split("\n")[0]
+
 # We'll name the database the same as the service.
 database_name, _ = change_unit.split("/")
 # A user per service unit so we can deny access quickly
@@ -13,6 +22,8 @@ lastrun_path = '/var/lib/ensemble/%s.%s.lastrun' % (database_name,user)
 slave_configured_path = '/var/lib/ensemble.slave.configured.for.%s' % database_name
 slave_configured = os.path.exists(slave_configured_path)
 slave = os.path.exists('/var/lib/ensemble/i.am.a.slave')
+broken_path = '/var/lib/ensemble/%s.mysql.broken' % database_name
+broken = os.path.exists(broken_path)
 
 def get_db_cursor():
     # Connect to mysql
