6b92533f0e210f664f9208438411b5f294723211
James Page <james.page@canonical.com>
2012-11-29 16:31:14 +0000

    Changed to the shared-db hook to allow creation of multiple users/databases in the same hook execution context.
    Todo this this hook has been re-implemented in python; existing interface
    functionality remains unchanged but the hook will also now accept requests
    for multiple username/databases at the same time. This was implemented to
    allow quantum and nova services to be hosted in the same charm without the
    requirement for additional relations or subordinates.
    
    Removed print of root password in juju log.

--- hooks/common.py
+++ hooks/common.py
@@ -59,8 +59,61 @@ broken = os.path.exists(broken_path)
 def get_db_cursor():
     # Connect to mysql
     passwd = open("/var/lib/juju/mysql.passwd").read().strip()
-    print passwd
     connection = MySQLdb.connect(user="root", host="localhost", passwd=passwd)
-
     return connection.cursor()
 
+
+def database_exists(db_name):
+    cursor = get_db_cursor()
+    try:
+        cursor.execute("SHOW DATABASES")
+        databases = [i[0] for i in cursor.fetchall()]
+    finally:
+        cursor.close()
+    return db_name in databases
+
+
+def create_database(db_name):
+    cursor = get_db_cursor()
+    try:
+        cursor.execute("CREATE DATABASE {}".format(db_name))
+    finally:
+        cursor.close()
+
+
+def grant_exists(db_name, db_user, remote_ip):
+    cursor = get_db_cursor()
+    try:
+        cursor.execute("SHOW GRANTS for '{}'@'{}'".format(db_user,
+                                                          remote_ip))
+        grants = [i[0] for i in cursor.fetchall()]
+    except MySQLdb.OperationalError:
+        print "No grants found"
+        return False
+    finally:
+        cursor.close()
+    return "GRANT ALL PRIVILEGES ON `{}`".format(db_name) in grants
+
+
+def create_grant(db_name, db_user,
+                 remote_ip, password):
+    cursor = get_db_cursor()
+    try:
+        cursor.execute("GRANT ALL PRIVILEGES ON {}.* TO '{}'@'{}' "\
+                       "IDENTIFIED BY '{}'".format(db_name,
+                                                   db_user,
+                                                   remote_ip,
+                                                   password))
+    finally:
+        cursor.close()
+
+
+def cleanup_grant(db_user,
+                  remote_ip):
+    cursor = get_db_cursor()
+    try:
+        cursor.execute("DROP FROM mysql.user WHERE user='{}' "\
+                       "AND HOST='{}'".format(db_user,
+                                              remote_ip))
+    finally:
+        cursor.close()
