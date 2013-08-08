2f5c50a7c92b8fcb5f803401b7a580952bda0895
Andreas Hasenack <andreas@canonical.com>
2013-08-08 18:50:40 -0300

    Wait for the rbd device to be available before attempting to format it.

--- hooks/lib/ceph_utils.py
+++ hooks/lib/ceph_utils.py
@@ -12,6 +12,7 @@ import commands
 import subprocess
 import os
 import shutil
+import time
 import lib.utils as utils
 
 KEYRING = '/etc/ceph/ceph.client.%s.keyring'
@@ -153,11 +154,22 @@ def filesystem_mounted(fs):
 
 
 def make_filesystem(blk_device, fstype='ext4'):
-    utils.juju_log('INFO',
-                   'ceph: Formatting block device %s as filesystem %s.' %\
-                   (blk_device, fstype))
-    cmd = ['mkfs', '-t', fstype, blk_device]
-    execute(cmd)
+    count = 0
+    e_noent = os.errno.ENOENT
+    while not os.path.exists(blk_device):
+        if count >= 10:
+            utils.juju_log('ERROR',
+                'ceph: gave up waiting on block device %s' % blk_device)
+            raise IOError(e_noent, os.strerror(e_noent), blk_device)
+        utils.juju_log('INFO',
+            'ceph: waiting for block device %s to appear' % blk_device)
+        count += 1
+        time.sleep(1)
+    else:
+        utils.juju_log('INFO',
+            'ceph: Formatting block device %s as filesystem %s.' %
+            (blk_device, fstype))
+        execute(['mkfs', '-t', fstype, blk_device])
 
 
 def place_data_on_ceph(service, blk_device, data_src_dst, fstype='ext4'):
